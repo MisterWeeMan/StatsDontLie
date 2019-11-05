f5121a281b77f948e7f62a999d4908eccc57c8cb
MattiaBano <mattia21bano@gmail.com>
2019-11-05 19:24:48 +0100

    Add player detail fragment. Add share button. Add animations.

--- app/src/main/java/com/example/statsdontlie/fragments/PlayersFragment.kt
+++ app/src/main/java/com/example/statsdontlie/fragments/PlayersFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 
 import com.example.statsdontlie.R
 import com.example.statsdontlie.databinding.FragmentPlayersBinding
@@ -22,6 +23,19 @@ class PlayersFragment : Fragment() {
         savedInstanceState: Bundle?): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_players, container, false)
+
+        binding.apply {
+            // FIXME
+            lebronDummyText.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_playersFragment_to_detailFragment)
+            )
+            adDummyText.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_playersFragment_to_detailFragment)
+            )
+            durantDummyText.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_playersFragment_to_detailFragment)
+            )
+        }
 
         return binding.root
     }
